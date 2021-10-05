e9a1d3dc9ece6b188af3ab7f00fb6a344a968343
Suraj Mittal <mittal.suraj18@gmail.com>
2021-10-05 08:20:37 +0000

    seconf commit

---
+++ build/lib/my_module/random_util.py
@@ -0,0 +1,6 @@
+def print_something():
+    print("Hello world")
+
+
+def new_fn():
+    print("nw fn")
--- setup.py
+++ setup.py
@@ -1,20 +1,29 @@
 from setuptools import setup, find_packages
 
-setup(
-    name = "my_package",
-    version = "0.1" , 
-    packages=find_packages(),
-    license = "No License",
-    author="suraj.mittal",
-    install_requires = ["pandas"]
-)
+with open("./req.txt", "r") as fi:
+    req = fi.read().split("\n")
 
+print(find_packages())
 
 setup(
-    name="my_packages_2",
-    packages=find_packages(where="src"),
+    name="my_package",
     version="0.1",
-    description="pacakging inside src folder",
+    packages=find_packages(),
+    license="No License",
     author="suraj.mittal",
-    package_dir={"": "src"},
+    install_requires=req,
+    python_requires=">=3.7",
+#     package_data={
+#         "sample": ["package_data.dat"],
+#     },
 )
+
+
+# setup(
+#     name="my_packages_2",
+#     packages=find_packages(where="src"),
+#     version="0.1",
+#     description="pacakging inside src folder",
+#     author="suraj.mittal",
+#     package_dir={"": "src"},
+# )
